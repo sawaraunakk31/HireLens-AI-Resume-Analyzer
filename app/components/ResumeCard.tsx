c2fe2834bb7e460af9a89eb3edbe36aef2db2d86
sawaraunakk31 <sawaraunakk31@gmail.com>
2026-02-28 00:57:58 +0530

    Fetch real resumes on the homepage

--- app/components/ResumeCard.tsx
+++ app/components/ResumeCard.tsx
@@ -1,7 +1,20 @@
+import { useEffect, useState } from "react";
 import { Link } from "react-router";
 import ScoreCircle from "~/components/ScoreCircle";
+import { usePuterStore } from "~/lib/puter";
 
 const ResumeCard = ({ resume: { id, companyName, jobTitle, feedback, imagePath } }: { resume: Resume }) => {
+    const {fs}=usePuterStore();
+    const [resumeUrl,setResumeUrl]=useState<string>('');
+    useEffect(()=>{
+            const loadResume=async()=>{
+                const blob=await fs.read(imagePath);
+                if(!blob)return;
+                let url=URL.createObjectURL(blob);
+                setResumeUrl(url);
+            }
+            loadResume();
+        },[imagePath]);
 
     return (
         <Link to={`/resume/${id}`} className="resume-card animate-in fade-in duration-1000">
@@ -15,11 +28,11 @@ const ResumeCard = ({ resume: { id, companyName, jobTitle, feedback, imagePath }
                     <ScoreCircle score={feedback.overallScore} />
                 </div>
             </div>
-            {imagePath && (
+            {resumeUrl && (
                 <div className="gradient-border animate-in fade-in duration-1000">
                     <div className="w-full h-full">
                         <img
-                            src={imagePath}
+                            src={resumeUrl}
                             alt="resume"
                             className="w-full h-[350px] max-sm:h-[200px] object-cover object-top"
                         />
